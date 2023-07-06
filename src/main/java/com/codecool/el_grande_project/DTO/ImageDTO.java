d7f37fdec39a5828128f139c6ec9dad990cbbacb
unknown <marcinrasala@gmail.com>
2023-07-06 20:43:53 +0200

    add multiple image to add legend

--- src/main/java/com/codecool/el_grande_project/DTO/ImageDTO.java
+++ src/main/java/com/codecool/el_grande_project/DTO/ImageDTO.java
@@ -2,12 +2,14 @@ package com.codecool.el_grande_project.DTO;
 
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter
 public class ImageDTO {
-    private Long place_id;
+    private UUID place_id;
     private String imageUrl;
 
-    public ImageDTO(Long place_id, String imageUrl) {
+    public ImageDTO(UUID place_id, String imageUrl) {
         this.place_id = place_id;
         this.imageUrl= imageUrl;
     }
--- src/main/java/com/codecool/el_grande_project/DTO/NewPlaceDTO.java
+++ src/main/java/com/codecool/el_grande_project/DTO/NewPlaceDTO.java
@@ -2,6 +2,7 @@ package com.codecool.el_grande_project.DTO;
 
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 @Getter
@@ -12,13 +13,15 @@ public class NewPlaceDTO {
     private double longitude;
     private String description;
     private String name;
+    private ArrayList<String> imageUrls;
 
-    public NewPlaceDTO(UUID userId, Long category_id, String description, String name, double longitude, double latitude) {
+    public NewPlaceDTO(UUID userId, Long category_id, String description, String name, double longitude, double latitude,ArrayList<String> imageUrls) {
         this.userId = userId;
         this.category_id = category_id;
         this.description = description;
         this.name = name;
         this.longitude = longitude;
         this.latitude=latitude;
+        this.imageUrls=imageUrls;
     }
 }
